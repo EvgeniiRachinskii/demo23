043a3b519e1c540f1367412a20bb0f81f77fee00
EvgenRatch <elchangizo@yandex.ru>
2023-05-17 13:55:56 +0300

    dw

--- src/main/java/com/example/demo23/Demo23Application.java
+++ src/main/java/com/example/demo23/Demo23Application.java
@@ -7,7 +7,33 @@ public class Demo23Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Demo23Application.class, args);
-
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 
 }
